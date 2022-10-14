634e3be81ae6bd7f9e6abba4c873f85964cadd79
kishan0204 <kishanmalaviya.km@gmail.com>
2022-10-14 01:19:08 +0100

    my homework

---
+++ src/programme/Programme_1_BioData.java
@@ -0,0 +1,26 @@
+package programme;
+
+/**
+ * 1.write a program that prints your BIO Data.
+ *
+ */
+
+public class Programme_1_BioData {
+    public static void main (String[] args){
+        System.out.println("              BIO DATA             ");
+        System.out.println("Name            : Jane Smith");
+        System.out.println("Mobile          : 07912345678");
+        System.out.println("Email           : Janesmith@gmail.com");
+        System.out.println("Gender          : Female");
+        System.out.println("Date of Birth   : 01.05.2000");
+        System.out.println("Marital Status  : Single");
+        System.out.println("Languages Know  : English, Spanish, and German");
+        System.out.println("Qualification   : Software Tester");
+        System.out.println("Experience      : Agile Tester with 5 year of experience");
+        System.out.println("Address         : 123 Abc Street, London Ab1 2CD");
+        System.out.println("Place           : London");
+        System.out.println("Date            : 02.10.2011");
+        System.out.println("Signature       : JSmith");
+
+    }
+}
---
+++ src/programme/programme_2_PrintFace.java
@@ -0,0 +1,16 @@
+package programme;
+
+public class programme_2_PrintFace {
+    /**
+     * 2. Write a Java program to print a face.
+     */
+    public static void main(String[]args){
+        System.out.println(" +\"\"\"\"\"+");
+        System.out.println("[| o o |]");
+        System.out.println(" |  ^  | ");
+        System.out.println(" | ._. | ");
+        System.out.println(" +_____+ ");
+
+    }
+}
+
